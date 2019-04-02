81a48d8012b61daf9e48c3dfe63c3f66780a43f8
Jerome <jerome.bonnier@coding-academy.fr>
2019-04-02 11:56:34 +0200

    Modif

--- Register/inc/db.php
+++ Register/inc/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:dbname=pool_php_rush;host=localhost', 'titi', '1234');
+$pdo = new PDO('mysql:dbname=pool_php_rush;host=localhost', 'titi', '1234', NULL);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
